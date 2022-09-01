267561aaa28a935ed3ef74128963cd26c2712eac
Prads-ai <63697168+Prads-ai@users.noreply.github.com>
2022-09-01 02:06:39 -0400

    Update Appointments.java
    Refactor the code

--- Appointments.java
+++ Appointments.java
@@ -17,39 +17,26 @@ import java.sql.Statement;
 public class Appointments {
     
 private String apptDateTime;
-                        private String patient_id;
-                        private String chiroprac_id;
-                        
+       private String patient_id;
+       private String chiroprac_id;
+       private String proc_code;
+       private String office_num;
                        
-                        private String proc_code;
-                        private String office_num;
-                       
-		
+public Appointments() {
+	apptDateTime="";
+        patient_id="";
+	chiroprac_id="";
+	proc_code="";
+	office_num="";
+}
 
-                public Appointments() {
-			apptDateTime="";
-                        patient_id="";
-			chiroprac_id="";
-			proc_code="";
-                        
-                        
-			office_num="";
-                    
-                       
-		}
-
-
-
-
-		public Appointments(String i, String fo, String ln, String em, String in ) {
-			
-                    apptDateTime = i;
-                    patient_id = fo;
-                    chiroprac_id=ln;
-                    
-                    proc_code=em;
-                    office_num=in;
-		}
+public Appointments(String i, String fo, String ln, String em, String in ) {
+	apptDateTime = i;
+        patient_id = fo;
+        chiroprac_id=ln;            
+        proc_code=em;
+        office_num=in;
+}
                 
   /*********************************************************************
  *
@@ -57,42 +44,36 @@ private String apptDateTime;
  * 
  *******************************************************************/              
                 
-                
-		public void selectDB(String i) {
-                                           patient_id=i;
-			try {    //Load DB Driver
-				Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-            			Connection con1 = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/Kevin/Downloads/ChiropractorOfficeMDB.accdb");
+ public void selectDB(String i) {
+       patient_id=i;
+	try {    
+		//Load DB Driver
+		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            	Connection con1 = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/Kevin/Downloads/ChiropractorOfficeMDB.accdb");
+		
+		//Execute SQL Statement
+		Statement stmt =con1.createStatement();
+		String sql = "Select * from Appointments where patient_id='"+getpatient_id()+"'";
+		 //Process ResultSet
+		System.out.println(sql);
+           	 ResultSet rs = stmt.executeQuery(sql);
+           	 rs.next();
 
-
-			           //Execute SQL Statement
-				Statement stmt =con1.createStatement();
-			        String sql = "Select * from Appointments where patient_id='"+getpatient_id()+"'";
-			          //Process ResultSet
-				System.out.println(sql);
-            ResultSet rs = stmt.executeQuery(sql);
-            rs.next();
+           	 setpatient_id(rs.getString(1));
+            	setchiroprac_id(rs.getString(2));
             
-            
-            
-           
-            setpatient_id(rs.getString(1));
-            setchiroprac_id(rs.getString(2));
-            
-            setproc_code(rs.getString(3));
-            setoffice_num(rs.getString(4));          
-			}
-			catch(Exception se) {
-				System.out.println(se);
-			}
-		} //end selectDB()
+            	setproc_code(rs.getString(3));
+           	 setoffice_num(rs.getString(4));          
+		}catch(Exception se) {
+			System.out.println(se);
+		}
+	} //end selectDB()
                 
-       public void updateDB(){
-         
-       
-        try{
+public void updateDB(){
+
+      try{
            Connection con1 = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/Kevin/Downloads/ChiropractorOfficeMDB.accdb");
-            Statement stmt = con1.createStatement();
+           Statement stmt = con1.createStatement();
         
             String sql = "update Appointments set apptDateTime = '"+getapptDateTime()+"', chiroprac_id = '"+getpatient_id()+"', proc_code = '"+getproc_code()+"', office_num = '"+getoffice_num()+"' where patient_id = '"+getpatient_id()+"'";
                     
@@ -110,53 +91,41 @@ private String apptDateTime;
         }
       
     }//end updateDB()          
-                    
-                
+//======================= Behaviors ===================================
+// Setters and Getters
 		
-		public String getapptDateTime() { return apptDateTime; }
-		public void setapptDateTime (String i) { apptDateTime=i; }
+public String getapptDateTime() { return apptDateTime; }
+public void setapptDateTime (String i) { apptDateTime=i; }
                 
-                
+public String getpatient_id() { return patient_id; }
+public void setpatient_id(String fo) { patient_id=fo; }
 		
-		public String getpatient_id() { return patient_id; }
-		public void setpatient_id(String fo) { patient_id=fo; }
-		
-		public String getchiroprac_id() { return chiroprac_id; }
-		public void setchiroprac_id(String ln) { chiroprac_id=ln; }
+public String getchiroprac_id() { return chiroprac_id; }
+public void setchiroprac_id(String ln) { chiroprac_id=ln; }
 
+public String getproc_code() { return proc_code; }
+public void setproc_code(String em) {proc_code=em; }
+
+public String getoffice_num() { return office_num; }
+public void setoffice_num(String in) { office_num=in; }
+
+// Display method - return all the values of the class fields .
+
+public void display() {
+	System.out.println("Date            =   "+ apptDateTime);
+	System.out.println("Patient ID     =   "+ patient_id);
+	System.out.println("Chiropractor ID    =   "+ chiroprac_id);
+	System.out.println("Code           =   "+ proc_code);
+        System.out.println("Office number           =   "+ office_num);
+}
 		
-		public String getproc_code() { return proc_code; }
-		public void setproc_code(String em) {proc_code=em; }
-		
-              public String getoffice_num() { return office_num; }
-		public void setoffice_num(String in) { office_num=in; }
-               
-   
-                
-                
-		public void display() {
-			System.out.println("Date            =   "+ apptDateTime);
-                       
-			System.out.println("Patient ID     =   "+ patient_id);
-			System.out.println("Chiropractor ID    =   "+ chiroprac_id);
+public static void main(String args[]) {
+
+	// Testing Database methods
 			
-			System.out.println("Code           =   "+ proc_code);
-                        System.out.println("Office number           =   "+ office_num);
-		}
-		
-		public static void main(String args[]) {
-			// Testing Database methods
-			
-                   Appointments a1;
-           a1 = new Appointments();
-	 a1.selectDB("P202");
-           a1.display();    
-                  
-             
-                        
-                      
-                }
-
-   
-
+Appointments a1;
+a1 = new Appointments();
+a1.selectDB("P202");
+a1.display();                      
+	}
 }
